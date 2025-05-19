3509b5d444868200e42d37970e0d834876138793
Muhammet Kaya <66886314+kayamuhammet@users.noreply.github.com>
2025-05-19 22:22:37 +0300

    create project and connect ef core

---
+++ PastryShop/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PastryShop.Controllers;
+
+public class HomeController : Controller
+{
+
+    public HomeController()
+    {
+        
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
