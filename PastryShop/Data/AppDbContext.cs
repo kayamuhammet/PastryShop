84866641839ddd1acd600da369a727b963276be5
Muhammet Kaya <66886314+kayamuhammet@users.noreply.github.com>
2025-06-02 09:16:25 +0300

    Update AppDbContext to use IdentityDbContext and added DbSets for Sliders, Offers, Abouts, Footers, Orders, FAQs, and Comments

--- PastryShop/Data/AppDbContext.cs
+++ PastryShop/Data/AppDbContext.cs
@@ -1,13 +1,21 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using PastryShop.Models;
 
 namespace PastryShop.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Offer> Offers { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Footer> Footers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<FAQ> FAQs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
     }
 }
